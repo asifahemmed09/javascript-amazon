ad09fe0ca9d434592cf974a907ab7df0509d2ae6
asifahemmed09 <asifahemmed.dev@gmail.com>
2025-12-14 16:19:17 +0600

    interactive cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,5 +78,11 @@ document.querySelectorAll('.add-to-cart-button').forEach((button) => {
         quantity: 1,
       });
     }
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.cart-quantity').innerHTML = cartQuantity;
   });
 });
